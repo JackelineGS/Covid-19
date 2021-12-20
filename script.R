1ac2b0b48e311afe1f3f6154ccc4afad79a44ae6
JackelineGS <jackeline.garcia1@unmsm.edu.pe>
2021-12-20 01:15:12 -0500

    update

--- script.R
+++ script.R
@@ -1,3 +1,25 @@
-1+2
-2+3
-3+8
+
+library(tidyverse)
+library(gapminder)
+
+fv_bd <- read_csv("departamentos.csv")
+openxlsx::write.xlsx(fv_bd, file = "BD2.xlsx")
+
+library(readxl)
+BD <- read_excel("BD.xlsx")
+View(BD)
+
+
+BD %>% 
+ggplot(mapping = aes(x = epi_week, y = log(AMAZONAS), colour = ESTADO)) + 
+  geom_line() +
+  geom_point() +
+  theme_minimal() +
+  labs(x = "Year",
+       y = "GDP per capita",
+       title = "GDP per capita on Five Continents", 
+       subtitle = "Individual countries shown in gray, trend in blue.")
+
+
+
+
